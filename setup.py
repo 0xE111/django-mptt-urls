2f1c877c6cc0463c77839ddb3b33399568d14ee0
Alex <alex@localhost>
2015-03-04 16:21:50 +0300

    Remove author info, update setup.py

--- setup.py
+++ setup.py
@@ -8,16 +8,14 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-mptt-urls',
-    version='2.0.0',
+    version='2.0.0a1',
     packages=find_packages(),
     include_package_data=True,
     install_requires=['django>=1.7', 'django-mptt>=0.6.1'],
-    license='MIT License',
+    license='MIT',
     description='Django app for creating hierarchical URLs associated with django-MPTT models.',
     long_description=README,
     url='https://github.com/c0ntribut0r/django-mptt-urls',
-    author='Alex',
-    author_email='',
     keywords='django mptt urls hierarchy clean friendly',
     classifiers=[
         'Environment :: Web Environment',
